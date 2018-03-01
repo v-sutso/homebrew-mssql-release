31788db78f2de65ec0a87bad51cd9d39226f09a0
bamboo <bamboo@odbcusers-Mac.local>
2018-03-01 15:15:54 -0800

    add version 17.0.0.5

--- Formula/msodbcsql17.rb
+++ Formula/msodbcsql17.rb
@@ -8,7 +8,7 @@ class Msodbcsql17 < Formula
 
   option "without-registration", "Don't register the driver in odbcinst.ini"
 
-  #conflicts_with "msodbcsql", :because =>"msodbcsql symlinks msodbcsql.h"
+
   depends_on "unixodbc"
   depends_on "openssl"
 
@@ -45,11 +45,9 @@ class Msodbcsql17 < Formula
   def install
     return false unless check_eula_acceptance?
 
-    
-
     chmod 0444, "lib/libmsodbcsql.17.dylib"
     chmod 0444, "share/msodbcsql17/resources/en_US/msodbcsqlr17.rll"
-    chmod 0644, "include/msodbcsql.h"
+    chmod 0644, "include/msodbcsql17/msodbcsql.h"
     chmod 0644, "odbcinst.ini"
     chmod 0644, "share/doc/msodbcsql17/LICENSE.txt"
     chmod 0644, "share/doc/msodbcsql17/RELEASE_NOTES"
