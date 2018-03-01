6dbf4d0a82262f3cf72b9fd200c47e9d0678128b
bamboo <bamboo@odbcusers-Mac.local>
2018-02-28 16:29:40 -0800

    add version 17.0.0.5

--- Formula/msodbcsql17.rb
+++ Formula/msodbcsql17.rb
@@ -54,7 +54,7 @@ class Msodbcsql17 < Formula
 
     (prefix/"include").install_symlink "msodbcsql.h" => "#{HOMEBREW_PREFIX}/include/msodbcsql.h"
     #(prefix/"share/doc/msodbcsql").install_symlink "LICENSE.txt" => "#{HOMEBREW_PREFIX}/share/doc/msodbcsql/LICENSE.txt"
-    #(prefix/"share/doc/msodbcsql").install_symlink "RELEASE_NOTES" => "#{HOMEBREW_PREFIX}/share/doc/msodbcsql/RELEASE_NOTES"
+    (prefix/"share/doc/msodbcsql").install_symlink "RELEASE_NOTES" => "#{HOMEBREW_PREFIX}/share/doc/msodbcsql/RELEASE_NOTES"
 
     cp_r ".", prefix.to_s
 
